d3e1f79b5cfee29f9554d1ffbf05dbddeeb45043
notaplayer4768 <thad.philpot@gmail.com>
2022-09-28 10:20:14 -0700

    Initial commit

---
+++ app/src/main/java/com/example/quizapp/Question.kt
@@ -0,0 +1,8 @@
+package com.example.quizapp
+
+data class Question(var currentQuestion: String,
+                    var currentAnswer: String,
+                    var option1 : String,
+                    var option2 : String,
+                    var option3 : String,
+                    var option4 : String)
