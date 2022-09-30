4d0c4d7cfb23aa280a8cceaa8846d9bbe2d95e00
notaplayer4768 <thad.philpot@gmail.com>
2022-09-30 09:15:12 -0700

    Initial commit

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.quizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
 class MainActivity : AppCompatActivity() {
+    companion object{
+        val TAG = "MainActivity"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val inputStream = resources.openRawResource(R.raw.questions)
+        val jsonString = inputStream.bufferedReader().use {
+            // the last line of the use function is returned
+            it.readText()
+        }
+        Log.d(TAG, "onCreate: $jsonString")
+        val gson = Gson()
+        val type = object : TypeToken<List<Question>>() { }.type
+        val questions = gson.fromJson<List<Question>>(jsonString, type)
+
     }
 }
